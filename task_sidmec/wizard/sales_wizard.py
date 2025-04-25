0710e3ddbb8a8906ddbdad117c94106249d2cae5
akshaya-sidmec <akshaya@sidmectech.in>
2025-04-25 22:09:04 +0530

    report

--- task_sidmec/wizard/sales_wizard.py
+++ task_sidmec/wizard/sales_wizard.py
@@ -5,9 +5,9 @@ class CustomerReport(models.TransientModel):
     _description = 'Salesman report Wizard'
 
     # salesperson = fields.Many2one("res.partner",String="Salesman")
-    users = fields.Many2one("res.users",String="Salesman")
-    from_date = fields.Date(String="From_date")
-    to_date = fields.Date(String="To_date")
+    users = fields.Many2one("res.users",string="Salesman")
+    from_date = fields.Date(string="From_date")
+    to_date = fields.Date(string="To_date")
 
     def sale_pdf_report(self):
         for rec in self:

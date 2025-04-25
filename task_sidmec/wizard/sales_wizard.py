3ffd43d12828ce90908a89bec84f37c2cea4b91d
akshaya-sidmec <akshaya@sidmectech.in>
2025-04-25 13:11:09 +0530

    sales_Wizard

--- task_sidmec/wizard/sales_wizard.py
+++ task_sidmec/wizard/sales_wizard.py
@@ -1,36 +1,17 @@
-# from odoo import fields, models
-#
-#
-# class CustomerReport(models.TransientModel):
-#     _name = 'sales.wizard'
-#     _description = 'Salesman Report'
-#
-#     salesperson = fields.Many2one("res.partner", String="Salesman")
-#     from_date = fields.Date(String="From_date")
-#     to_date = fields.Date(String="to_date")
-#
-#     def view_salesman_report(self):
-#         rec = self  # or self.ensure_one() to be safer
-#         partner = self.env['sale.order'].search([('partner_id', '=', rec.salesperson.id)])
-#         data = {
-#             'sale_order': partner
-#         }
-#         report_action = self.env.ref('task_sidmec.report_salesman')
-#         return report_action.report_action(self, data=data)
-from odoo import models, fields
+from odoo import fields, models
 
+class CustomerReport(models.TransientModel):
+    _name = 'sale.report.wizard'
+    _description = 'Salesman report Wizard'
 
-class SaleOrderWizard(models.TransientModel):
-    _name = "sale.report.wizard"
-    _description = "Sale Report Wizard"
-
-    partner_id = fields.Many2one("res.partner", string="Salesman")
-    from_date = fields.Date("From Date")
-    to_date = fields.Date("To Date")
+    # salesperson = fields.Many2one("res.partner",String="Salesman")
+    users = fields.Many2one("res.users",String="Salesman")
+    from_date = fields.Date(String="From_date")
+    to_date = fields.Date(String="To_date")
 
     def sale_pdf_report(self):
         for rec in self:
-            domain = [('partner_id', '=', rec.partner_id.id)]
+            domain = [('user_id', '=', rec.users.id)]
 
             if rec.from_date:
                 domain.append(('date_order', '>=', rec.from_date))
@@ -46,16 +27,16 @@ class SaleOrderWizard(models.TransientModel):
                     {
                         'name': order.name,
                         'amount_total': order.amount_total,
-                        'sales_person': order.user_id.name,
+                        'sales_person': order.partner_id.name,
                     }
                     for order in sale_orders
                 ],
                 'wizard': {
-                    'partner_name': rec.partner_id.name,
+                    'partner_name': rec.users.name,
                     'from_date': rec.from_date,
                     'to_date': rec.to_date,
                     'total_amount': total_amount,
                 }
             }
 
-            return self.env.ref('task_sidmec.sale_wizard_report_pdf').report_action(self, data=data)
+            return self.env.ref('task_sidmec.report_wizard_salesperson_pdf').report_action(self, data=data)

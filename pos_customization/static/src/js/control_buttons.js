722b14fa36f77144dd5b16d01975d71ac8fe0207
akshaya-sidmec <akshaya@sidmectech.in>
2025-05-02 17:49:30 +0530

    button

--- pos_customization/static/src/js/control_buttons.js
+++ pos_customization/static/src/js/control_buttons.js
@@ -1,9 +1,22 @@
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { patch } from "@web/core/utils/patch";
+//import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
 
-patch(ControlButtons, {
-    components: {
-        ...ControlButtons.components,
-        Custom_Button,
+patch(ControlButtons.prototype, {
+    onClickQuotation() {
+//        this.dialog.add(SelectCreateDialog, {
+//            resModel: "sale.order",
+//            noCreate: true,
+//            multiSelect: false,
+//            domain: [
+//                ["state", "!=", "cancel"],
+//                ["invoice_status", "!=", "invoiced"],
+//                ["currency_id", "=", this.pos.currency.id],
+//            ],
+//            onSelected: async (resIds) => {
+//                await this.pos.onClickSaleOrder(resIds[0]);
+//            },
+//        });
+            confirm("Iam your custom buttom 'you can customize me!'")
     },
-})
+});
